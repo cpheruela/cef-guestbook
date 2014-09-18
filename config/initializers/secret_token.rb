27c7bc688a2c8c99339e0ad321ae1410fc0e2996
cpheruela <cef.heruela@gmail.com>
2014-09-18 09:36:03 -0700

    Created a rails application for guestbook

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guestbook::Application.config.secret_token = '70acf519cafdb311b44f93e7f433cbcbfbe68a75539200b0ca5bdef6f1366bc5dec2ea6dcbd74bf11e483cff1819102d7500c4cf210b50920b9ae6275e168141'
